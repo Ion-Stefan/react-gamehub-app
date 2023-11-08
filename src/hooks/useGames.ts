8c5f0025a0c3133deb85ab7cc5b0103f55a80a35
Ion Stefan <ion.adrian.stefann@gmail.com>
2023-11-08 14:44:45 +0100

    Built game card component

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
   // description: string;
-  // image: string;
+  background_image: string;
   // price: number;
   // release_date: string;
 }
