da3933f2ef5f508ea44a7c2594f6414f259dea12
Ion Stefan <ion.adrian.stefann@gmail.com>
2023-11-08 15:07:17 +0100

    Displaying GameCard platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import {
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+function PlatformIconList({ platforms }: Props) {
+  const iconMap: { [key: string]: IconType } = {
+    playstation: FaPlaystation,
+    pc: FaWindows,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack alignItems={"center"} marginY={2}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+interface Platform {
+  name: string;
+  slug: string;
+  id: number;
+}
+
 export interface Game {
   id: number;
   name: string;
-  // description: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
+  // description: string;
   // price: number;
   // release_date: string;
 }
