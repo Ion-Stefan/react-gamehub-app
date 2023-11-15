ff5e017544246a99ac15237cd9b0ac7581e05991
Ion Stefan <ion.adrian.stefann@gmail.com>
2023-11-15 13:04:56 +0100

    Refactored fetchGames with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,12 +9,12 @@ interface Props {
 }
 
 function GameGrid({ gameQuery }: Props) {
-  const { data, error, loading } = useGames(gameQuery);
+  const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
 
-  if (error) return <Text>"Request failed with status of 404"</Text>;
+  if (error) return <Text>{error.message}</Text>;
 
   return (
     <SimpleGrid
@@ -22,9 +22,9 @@ function GameGrid({ gameQuery }: Props) {
       padding={10}
       spacing={10}
     >
-      {loading &&
+      {isLoading &&
         skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCard key={game.id} game={game} />
       ))}
     </SimpleGrid>
