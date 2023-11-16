164028f62c867396cea2575612423d7b61fc0dc3
Ion Stefan <ion.adrian.stefann@gmail.com>
2023-11-16 12:55:56 +0100

    Modified GameCard Skeleton

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,4 +1,10 @@
-import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Skeleton,
+  SkeletonText,
+  SkeletonCircle,
+} from "@chakra-ui/react";
 
 function GameCardSkeleton() {
   return (
@@ -6,6 +12,7 @@ function GameCardSkeleton() {
       <Card borderRadius={10} overflow={"hidden"} boxShadow={"dark-lg"}>
         <Skeleton height={"300px"} width={"300px"}>
           <CardBody>
+            <SkeletonCircle />
             <SkeletonText />
           </CardBody>
         </Skeleton>
